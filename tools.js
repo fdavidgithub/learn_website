e6649cf8c408ecbe9d06cbb223d20c2f730df10e
Francis David <contato@fdavid.com.br>
2018-07-17 11:51:34 -0300

    New records in array

--- tools.js
+++ tools.js
@@ -8,15 +8,20 @@ var groups = [
     {id: 4, name:"Databases"},
     {id: 5, name:"Embeddeds"},
     {id: 6, name:"Science Computer"},
-    {id: 7, name:"Others"}
+    {id: 7, name:"Others"},
+    {id: 8, name:"Servers"}
 ];
 
 var techs = [
+//    {name: "Windows", id_group: 1, year: 1990},
     {name: "Linux", id_group: 1, year: 2000, projects: 1},
     {name: "Android", id_group: 1},
 
-    {name: "Clipper", id_group: 2, year: 1997, projects: 35},
-    {name: "Delphi", id_group: 2, year: 2001, projects: 112},
+//    {name: "Basic", id_group: 2, year: 1995},
+//    {name: "Visual Basic", id_group: 2, year: 1996},
+//    {name: "Clipper", id_group: 2, year: 1997, projects: 35},
+//    {name: "Delphi", id_group: 2, year: 2001, projects: 112},
+//    {name: "PHP", id_group: 2},
     {name: "C", id_group: 2, year: 2018, projects: 1},
     {name: "C++", id_group: 2},
     {name: "Python", id_group: 2},
@@ -25,13 +30,15 @@ var techs = [
 
     {name: "Odoo", id_group: 3, year: 2018, projects: 2},
     {name: "Django", id_group: 3},
-    {name: "AWS", id_group:     3},
+   
+    {name: "AWS", id_group: 8},
+    {name: "Mosquitto", id_group: 8},
 
     {name: "PostgreSQL", id_group: 4, year: 2017, projects: 1},
     {name: "MySQL", id_group: 4},
     {name: "SQLite", id_group:4, year: 2018, projects: 1},
     {name: "MongoDB", id_group: 4},
-    {name: "Firebird", id_group: 4, year: 2001, projects: 100},
+//    {name: "Firebird", id_group: 4, year: 2001, projects: 100},
 
     {name: "Raspberry Pi", id_group: 5, year: 2018, projects: 1},
     {name: "Arduino", id_group: 5, year: 2017, projects: 4},
@@ -42,8 +49,8 @@ var techs = [
 
     {name: "HTML", id_group: 7, year: 2015 , projects: 8},
     {name: "CSS", id_group:7, year: 2018, project: 1},
-    {name: "JSON", id_group: 7, year: 2018, projects: 1}
-
+    {name: "JSON", id_group: 7, year: 2018, projects: 1},
+    {name: "MQTT", id_group: 7, year: 2018}
 ]
 
 for(let i=0; i<groups.length; i++) {
